4d5ea6d8ff7c25ede9bab9836937276bfa2ded88
Patrick Blatter <patrick_blatter@bluewin.ch>
2018-04-26 16:56:40 +0200

    Added Rental Schema

---
+++ server/models/rental.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const { ObjectId } = Schema.Types;
+const rentalSchema = new Schema({
+  userId: {
+    type: ObjectId,
+    required: true
+  },
+  boatId: {
+    type: ObjectId,
+    required: true
+  },
+  rentalStartDate: {
+    type: Date,
+    required: true
+  },
+  rentalEndDate: {
+    type: Date,
+    required: true
+  },
+  pricePerDay: {
+    type: Number,
+    required: true
+  },
+  totalPrice: {
+    type: Number
+  }
+});
+
+const Rental = mongoose.model('rental', rentalSchema);
+
+rentalSchema.pre('save', function(next) {
+  const start = Date.parse(this.rentalStartDate);
+  const end = Date.parse(this.rentalEndDate);
+  const oneDay = 1000 * 60 * 60 * 24;
+  const difference = (end - start) / oneDay;
+
+  if (difference <= 0){
+    // NOT POSSIBLE
+  } else {
+    // calculate total price
+    this.totalPrice = this.pricePerDay * difference;
+  }
+});
+
+
+
+
+module.exports = Rental;
