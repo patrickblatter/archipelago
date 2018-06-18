16c371e01351cd671658c636fb550d078b462508
Patrick Blatter <patrick.blatter93@gmail.com>
2018-06-18 16:32:40 +0200

    added prod vars

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,3 +1,7 @@
 module.exports = {
     PORT: process.env.PORT,
+    MONGO_URI: process.env.MONGO_URI,
+    JWT_SECRET: process.env.JWT_SECRET,
+    IAM_ACCESS_KEY: process.env.IAM_ACCESS,
+    IAM_SECRET_KEY: process.env.IAM_SECRET
 }
